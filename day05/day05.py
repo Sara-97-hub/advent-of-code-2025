7ba25e4b97151ca64465e9d2b50b8719f6e27c50
Sara-97-hub <Saradavidovic111@gmail.com>
2025-12-08 19:46:40 +0100

    Day 5 redone

--- day05/day05.py
+++ day05/day05.py
@@ -1,33 +1,50 @@
-def count_rolls(grid, r, c):
-    rows = len(grid)
-    cols = len(grid[0])
-    count = 0
+def read_database(ingredient_file):
+   
+    lines = []
 
-    for rr in range(r - 1, r + 2):
-        for cc in range(c - 1, c + 2):
-
-            if rr == r and cc == c:
-                continue
-
-            if rr < 0 or rr >= rows or cc < 0 or cc >= cols:
-                continue
-
-            if grid[rr][cc] == '@':
-                count += 1
-    
-    return count
+    with open(ingredient_file, "r") as f:
+       for line in f:
+         lines.append(line.strip())
 
 
-grid = []
-for line in open('input05.txt'):
-    grid.append(list(line.strip()))
+    blank_index = lines.index("")
 
-total_rolls = 0
+    range_lines = lines[:blank_index]
+    ingredient_lines = lines[blank_index + 1:]
 
-for r in range(len(grid)):
-    for c in range(len(grid[0])):
-        if grid[r][c] == '@':
-            if count_rolls(grid, r, c) < 4:
-                total_rolls += 1
+    ranges = []
+    for line in range_lines:
+        start_str, end_str = line.split("-")
+        start = int(start_str)
+        end = int(end_str)
+        ranges.append((start, end))
 
-print(total_rolls)
+    ingredient_ids = []
+    for line in ingredient_lines:
+        if line != "":
+            ingredient_ids.append(int(line))
+
+    return ranges, ingredient_ids
+
+
+def main():
+    ranges,ingredient_ids = read_database("input05.txt")
+
+    fresh_count = 0
+
+    for ingredient_id in ingredient_ids:
+        is_fresh = False
+
+        for start, end in ranges:
+            if start <= ingredient_id <= end:
+                is_fresh = True
+                break
+
+        if is_fresh:
+            fresh_count += 1
+
+    print(fresh_count)
+
+
+if __name__ == "__main__":
+    main()
