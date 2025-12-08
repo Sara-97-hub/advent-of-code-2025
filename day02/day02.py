d138136ae329e9fa0c30d8bcef51ab9d38b72a73
Sara-97-hub <Saradavidovic111@gmail.com>
2025-12-08 18:53:15 +0100

    Added Part 2 solution

--- day02/day02.py
+++ day02/day02.py
@@ -1,26 +1,28 @@
 def is_invalid_id(num: int) -> bool:
     s = str(num)
-    if len(s) % 2 != 0:
-        return False
-    half = len(s) // 2
-    return s[:half] == s[half:]
+    L = len(s)
+    for size in range(1, L // 2 + 1):
+        if L % size != 0:
+            continue
+        pattern = s[:size]
+        repeats = L // size
+        if repeats < 2:
+            continue
+        if pattern * repeats == s:
+            return True
+    return False
 
 
 def main():
     total = 0
-
     with open("input02.txt", "r") as f:
         data = f.read().strip()
-
     ranges = data.split(",")
-
     for r in ranges:
         start, end = map(int, r.split("-"))
-
         for n in range(start, end + 1):
             if is_invalid_id(n):
                 total += n
-
     print("Result (sum of invalid IDs):", total)
 
 
